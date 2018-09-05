32ae365b1d090f600f8d4fffc33f196d849c6e20
Jennifer O'Kane <j.o'kane.17@ucl.ac.uk>
2018-09-05 11:14:10 +0100

    Add initial implementation of temperature conversion

---
+++ conversion.py
@@ -0,0 +1,11 @@
+def kelvin_to_celcius(temp):
+	return temp - 273.15
+
+def celcius_to_fahr(temp):
+	return temp * (9/5) +32
+
+def kelvin_to_fahr(temp):
+	temp_c = kelvin_to_celcius(temp)
+	result = celcius_to_fahr(temp_c)
+	return result 
+
